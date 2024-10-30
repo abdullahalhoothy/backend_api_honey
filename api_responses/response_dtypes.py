dcd41952690c8cab9e34566604f1ceb8ff230c29
Ahmed Kamal <ahmedkamal200427@gmail.com>
2024-10-30 18:23:03 +0300

    feat: Add types for response in product details

---
+++ api_responses/response_dtypes.py
@@ -0,0 +1,43 @@
+from typing import Dict, List, TypeVar, Generic, Literal, Any, Optional
+
+from pydantic import BaseModel, Field
+
+T = TypeVar("T")
+
+
+class ResModel(BaseModel, Generic[T]):
+    message: str
+    request_id: str
+    data: T
+
+
+class UserRating(BaseModel):
+    rating: str
+    review: str
+    username: str
+    description: Optional[str] = None
+    userimageurl: Optional[str] = None
+
+
+class Product(BaseModel):
+    id: str
+    name: str
+    description: str
+    tagline: str
+    producturl: str
+    imageurl: str
+    ratingtext: str
+    ratingvalue: Optional[str] = None
+    averagerating: Optional[str] = None
+    totalratings: str
+    discountedprice: str
+    discountpercentage: str
+    originalprice: str
+    city: str
+    country: str
+    countryflagurl: str
+    userrating: UserRating
+
+
+class ProductDetail(BaseModel):
+    product: Product
--- database_transformations/product.py
+++ database_transformations/product.py
@@ -5,24 +5,26 @@ from database_transformations.product_schema import SCHEMA
 from database_transformations.sample_product_data import SAMPLE_PRODUCT
 
 
-
 async def create_product_table() -> None:
     # Create table if it doesn't exist
 
-    columns = ''.join([f'{k} {v},' for k, v in SCHEMA.items()]).strip(',')
+    columns = "".join([f"{k} {v}," for k, v in SCHEMA.items()]).strip(",")
     create_table_query = f"""
     CREATE TABLE IF NOT EXISTS Product (
             {columns}
     );
     """
     await Database.execute(create_table_query)
-    columns = ''.join([f'{k}, ' for k in SCHEMA.keys()]).rstrip(', ')
+    columns = "".join([f"{k}, " for k in SCHEMA.keys()]).rstrip(", ")
     await insert_product(columns)
+
 
 async def insert_product(columns: str) -> None:
     # Create table if it doesn't exist
     entries = SAMPLE_PRODUCT
-    placeholders = ''.join([f'${i + 1}, ' for i in range(len(entries[0].values()))]).rstrip(', ')
+    placeholders = "".join(
+        [f"${i + 1}, " for i in range(len(entries[0].values()))]
+    ).rstrip(", ")
     entries = [list(entry.values()) for entry in entries]
     query = f"""
         INSERT INTO Product ({columns})  
@@ -32,29 +34,29 @@ async def insert_product(columns: str) -> None:
     await Database.execute_many(query, entries)
 
 
-async def get_recommended_products(req: Optional[object]) -> dict:
+async def get_recommended_products(req: Optional[object] = None) -> dict:
     # Create table if it doesn't exist
     query = f"""
     SELECT * FROM Product
     WHERE id='66faa130bedf3403197df77c'            
     ;"""
     row = dict((await Database.fetchrow(query)).items())
-    row['userrating'] = json.loads(row['userrating'])
-    data = {
-        'justForYou': row,
-        'bestPick': row,
-        'bannerImageUrl': "https://"
-    }
-    return  data
+    row["userrating"] = json.loads(row["userrating"])
+    data = {"justForYou": row, "bestPick": row, "bannerImageUrl": "https://"}
+    return data
 
-async def get_preference_product_detail(req: Optional[object]) -> dict:
+
+async def get_preference_product_detail(req: Optional[object] = None) -> dict:
     # Create table if it doesn't exist
     query = f"""
     SELECT * FROM Product
     WHERE id='66faa130bedf3403197df77d'            
     ;"""
-    row = dict((await Database.fetchrow(query)).items())
-    row['userrating'] = json.loads(row['userrating'])
-    return  {
-        'product': row
-    }
+
+    row = await Database.fetchrow(query)
+
+    dict_data = dict(row)
+
+    dict_data["userrating"] = json.loads(dict_data["userrating"])
+
+    return {"product": dict_data}
