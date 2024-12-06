558e21395579e77734721fbf162b919b6763156f
shehramtahir <shehram.tahir@arbisoft.com>
2024-12-06 20:32:44 +0500

    FIX: Product id changed

--- database_transformations/product.py
+++ database_transformations/product.py
@@ -74,10 +74,10 @@ async def get_favorite_products(req: Optional[object] = None) -> List[Dict]:
     ## Waiting for Logic
     data = {
         "products": [
-            {"id": 101, "name": "Espresso", "searchType": "scan"},
-            {"id": 102, "name": "Cappuccino", "searchType": "search"},
-            {"id": 103, "name": "Latte", "searchType": "scan"},
-            {"id": 104, "name": "Americano", "searchType": "search"},
+            {"product_id": 101, "name": "Espresso", "searchType": "scan"},
+            {"product_id": 102, "name": "Cappuccino", "searchType": "search"},
+            {"product_id": 103, "name": "Latte", "searchType": "scan"},
+            {"product_id": 104, "name": "Americano", "searchType": "search"},
         ]
     }
     return data
@@ -87,7 +87,7 @@ async def get_user_reviews(req: Optional[object] = None) -> dict:
     data = {
         "reviews": [
             {
-                "id": 201,
+                "review_id": 201,
                 "name": "John Doe",
                 "userProfileImageUrl": "https://example.com/images/john_doe.jpg",
                 "description": "Great product, highly recommend!",
@@ -96,7 +96,7 @@ async def get_user_reviews(req: Optional[object] = None) -> dict:
                 "time": "2 months ago",
             },
             {
-                "id": 202,
+                "review_id": 202,
                 "name": "Jane Smith",
                 "userProfileImageUrl": "https://example.com/images/jane_smith.jpg",
                 "description": "Good quality, but a bit overpriced.",
@@ -105,7 +105,7 @@ async def get_user_reviews(req: Optional[object] = None) -> dict:
                 "time": "2 months ago",
             },
             {
-                "id": 203,
+                "review_id": 203,
                 "name": "Alex Johnson",
                 "userProfileImageUrl": "https://example.com/images/alex_johnson.jpg",
                 "description": "Exceeded my expectations. Will buy again!",
@@ -114,7 +114,7 @@ async def get_user_reviews(req: Optional[object] = None) -> dict:
                 "time": "2 months ago",
             },
             {
-                "id": 204,
+                "review_id": 204,
                 "name": "Emily Brown",
                 "userProfileImageUrl": "https://example.com/images/emily_brown.jpg",
                 "description": "Not as described, disappointed.",
@@ -143,37 +143,37 @@ async def get_coffee_types(req: Optional[object] = None) -> dict:
     data = {
         "products": [
             {
-                "id": 1,
+                "product_id": 1,
                 "type": "Espresso",
                 "imageUrl": "hhttps://storage.googleapis.com/vivi_app/postgreSQL/dbo-coffee/raw_schema_app_generic/banners/9.jpeg",
             },
             {
-                "id": 2,
+                "product_id": 2,
                 "type": "Latte",
                 "imageUrl": "https://storage.googleapis.com/vivi_app/postgreSQL/dbo-coffee/raw_schema_app_generic/banners/9.jpeg",
             },
             {
-                "id": 3,
+                "product_id": 3,
                 "type": "Cappuccino",
                 "imageUrl": "https://storage.googleapis.com/vivi_app/postgreSQL/dbo-coffee/raw_schema_app_generic/banners/10.jpeg",
             },
             {
-                "id": 4,
+                "product_id": 4,
                 "type": "Americano",
                 "imageUrl": "https://storage.googleapis.com/vivi_app/postgreSQL/dbo-coffee/raw_schema_app_generic/banners/10.jpeg",
             },
             {
-                "id": 5,
+                "product_id": 5,
                 "type": "Mocha",
                 "imageUrl": "https://storage.googleapis.com/vivi_app/postgreSQL/dbo-coffee/raw_schema_app_generic/banners/10.jpeg",
             },
             {
-                "id": 6,
+                "product_id": 6,
                 "type": "Macchiato",
                 "imageUrl": "https://storage.googleapis.com/vivi_app/postgreSQL/dbo-coffee/raw_schema_app_generic/banners/8.jpeg",
             },
             {
-                "id": 7,
+                "product_id": 7,
                 "type": "Flat White",
                 "imageUrl": "https://storage.googleapis.com/vivi_app/postgreSQL/dbo-coffee/raw_schema_app_generic/banners/8.jpeg",
             },
@@ -321,7 +321,7 @@ async def get_filtered_products(req: Optional[object] = None) -> dict:
     data = {
         "products": [
             {
-                "id": "cb0545375af050c698f8cc98ad5f0628",
+                "product_id": "cb0545375af050c698f8cc98ad5f0628",
                 "name": "Nescafe Classic Instant Coffee Jar 190g",
                 "description": "Experience a distinct and premium coffee taste with this imported soluble coffee powder. Crafted from the finest blend of Robusta and Arabica beans, each cup offers a rich and smooth flavor, expertly prepared to make your coffee moments truly special. The specially designed glass jar ensures that your Nescafe Gold stays fresh and flavorful to the last drop, preserving its aroma and taste for a consistently satisfying experience.",
                 "tagline": "Features:\nFlavor Profile: Rich and robust flavor with a smooth finish, perfect for any time of day.\nInstant Convenience: Quickly dissolves in hot water, allowing for a fast and easy coffee preparation.\nQuality Beans: Made from high-quality coffee beans, expertly roasted to deliver a classic coffee taste.\nVersatile Use: Can be enjoyed as a hot beverage or used as a base for cold coffee drinks, desserts, and recipes.\n\nBrewing Instructions:\nMeasure: Use 1-2 teaspoons of instant coffee per cup (200ml) of hot water, adjusting according to your taste preference.\nMix: Stir the coffee granules in hot water until fully dissolved.\nServe: Enjoy as is, or customize with milk, sugar, or flavored syrups as desired.\n\nStorage:\nKeep in a cool, dry place and ensure the jar is tightly sealed after use to maintain freshness.\n",
@@ -344,7 +344,7 @@ async def get_filtered_products(req: Optional[object] = None) -> dict:
                 },
             },
             {
-                "id": "a848b13fa88a4cf5fb96e9e2a1827f0a",
+                "product_id": "a848b13fa88a4cf5fb96e9e2a1827f0a",
                 "name": "Nescafe Gold Decaf Instant Coffee Jar 95g",
                 "description": "Enjoy the rich, smooth taste of Nescafe Gold without the caffeine. Made from carefully selected Arabica and Robusta beans, this premium decaf blend delivers the same great flavor you love. The 95g jar ensures freshness and convenience, perfect for a relaxing cup of coffee at any time of the day. Ideal for those seeking a high-quality coffee experience without the caffeine kick",
                 "tagline": "Features:\nDecaffeinated: Crafted using a special decaffeination process to retain the rich flavor while removing caffeine.\nRich Flavor: Offers a smooth and well-balanced taste profile with a hint of sweetness, ideal for coffee lovers looking for a caffeine-free option.\nInstant Convenience: Easily dissolves in hot water, providing a quick and hassle-free coffee experience.\nVersatile Use: Great for hot or cold coffee drinks, and can also be used in various recipes for desserts or baking.\n\nBrewing Instructions:\nMeasure: Use 1-2 teaspoons of Nescafe Gold Decaf per cup (200ml) of hot water, adjusting according to taste.\nMix: Stir the granules into hot water until fully dissolved.\nServe: Enjoy as is, or enhance with milk, sugar, or flavorings as desired.\n\nStorage:\nStore in a cool, dry place and keep the jar tightly sealed after use to preserve freshness.\n",
@@ -367,7 +367,7 @@ async def get_filtered_products(req: Optional[object] = None) -> dict:
                 },
             },
             {
-                "id": "4ed95bc7fcd498bfa70ee0205afc3aae",
+                "product_id": "4ed95bc7fcd498bfa70ee0205afc3aae",
                 "name": "Nescafe 3in1 Instant Coffee Mix Stick 20g (30+5 Sticks)",
                 "description": "Nescafe Instant Coffee Sticks are convenient single-serve packets of instant coffee, perfect for on-the-go coffee lovers. Each stick contains a blend of rich coffee that delivers the classic Nescafe taste with ease.\n",
                 "tagline": "Features:\nSingle-Serve Convenience: Each stick contains just the right amount of instant coffee for one cup, making it easy to enjoy coffee anywhere.\nRich Flavor: Delivers the familiar smooth and aromatic taste that Nescafe is known for.\nQuick Preparation: Simply mix with hot water for an instant coffee experience in seconds.\nVersatile: Ideal for home, work, travel, or outdoor activities.\n\nBrewing Instructions:\nOpen: Tear open a stick.\nMix: Empty the contents into a cup of hot water (about 200 ml).\nStir: Mix well until the coffee dissolves.\nServe: Enjoy plain or customize with milk, sugar, or your favorite flavors.\n\nStorage:\nStore in a cool, dry place. Reseal the box after use to maintain freshness.\n",
@@ -390,7 +390,7 @@ async def get_filtered_products(req: Optional[object] = None) -> dict:
                 },
             },
             {
-                "id": "2b579cbecdccb3a34350514ba5338252",
+                "product_id": "2b579cbecdccb3a34350514ba5338252",
                 "name": "Nescafe 2in1 Classic 11.7g, Pack of 30",
                 "description": "Nescafé 2in1 Classic offers a convenient blend of rich instant coffee and creamer in a single sachet. Its perfect for those who enjoy a quick, easy, and balanced coffee experience without the need for added sugar.",
                 "tagline": "Brand: Nescafe\nProduct Name: 2in1 Classic\nPackaging: Pack of 30 sachets\nSachet Size: 11.7g each\nIngredients: Instant coffee, sugar, and non-dairy creamer.\nFlavor Profile:\nSmooth and balanced with a rich coffee taste.\nSweetened to provide a delightful flavor without any additional ingredients.\nPreparation Method:\nSimply empty the sachet into a cup.\nAdd hot water, stir well, and enjoy.\nConvenience:\nIdeal for on-the-go, at the office, or at home.\nNo need for measuring or additional ingredients, making it perfect for busy lifestyles.",
@@ -413,7 +413,7 @@ async def get_filtered_products(req: Optional[object] = None) -> dict:
                 },
             },
             {
-                "id": "744444889fbb91770001b4c6605497e1",
+                "product_id": "744444889fbb91770001b4c6605497e1",
                 "name": "Nescafe Gold Dark Roast Jar 190g",
                 "description": "Nescafé Gold Dark Roast is crafted from a premium blend of high-quality, dark-roasted Arabica and Robusta coffee beans. This instant coffee offers an intense, rich, and full-bodied flavor with a robust aroma, perfect for those who enjoy a deep coffee experience. Its designed to deliver a superior taste with a refined finish, making it ideal for a premium coffee moment at home.",
                 "tagline": "Brand: Nescafe\nProduct Name: Gold Dark Roast\nPackaging: Jar\nNet Weight: 190g\nIngredients: Instant coffee.\nFlavor Profile:\nIntense and bold with a deep roasted flavor.\nSmooth texture with a rich aroma that enhances the coffee experience.\nPreparation Method:\nAdd 1-2 teaspoons of coffee to a cup.\nPour hot water and stir until dissolved. Enjoy your coffee!\nVersatility:\nPerfect for making classic coffee or as a base for various coffee recipes.\nCan be enjoyed black or with milk and sugar to taste.",
@@ -436,7 +436,7 @@ async def get_filtered_products(req: Optional[object] = None) -> dict:
                 },
             },
             {
-                "id": "e81bbd7cd6bb6dd0fa6a06ec69ccc144",
+                "product_id": "e81bbd7cd6bb6dd0fa6a06ec69ccc144",
                 "name": "Nescafe Classic Instant Coffee Jar 95g",
                 "description": "Nescafé Classic Instant Coffee offers a rich, robust, and aromatic coffee experience, made from 100% pure coffee beans. Carefully selected and roasted to perfection, this instant coffee is ideal for those who enjoy a bold and strong flavor. It is designed to deliver a consistent and rich cup of coffee with a smooth finish, providing the perfect start to your day or a refreshing break anytime.\n",
                 "tagline": "Brand: Nescafe\nProduct Name: Classic Instant Coffee\nPackaging: Jar\nNet Weight: 95g\nIngredients: 100% instant coffee.\nFlavor Profile:\nClassic and balanced flavor with a smooth finish.\nRich aroma that awakens the senses.\nPreparation Method:\nAdd 1-2 teaspoons of instant coffee to a cup.\nPour hot water and stir well until dissolved. Enjoy your coffee!\nVersatility:\nIdeal for everyday coffee consumption.\nCan be enhanced with milk, cream, or sugar according to personal preference.",
@@ -465,7 +465,7 @@ async def get_filtered_products(req: Optional[object] = None) -> dict:
 
 async def get_single_user_review(req: Optional[object] = None) -> dict:
     data = {
-        "id": "e81bbd7cd6bb6dd0fa6a06ec69ccc144",
+        "product_id": "e81bbd7cd6bb6dd0fa6a06ec69ccc144",
         "name": "Nescafe Classic Instant Coffee Jar 95g",
         "description": "Nescafé Classic Instant Coffee offers a rich, robust, and aromatic coffee experience, made from 100% pure coffee beans. Carefully selected and roasted to perfection, this instant coffee is ideal for those who enjoy a bold and strong flavor. It is designed to deliver a consistent and rich cup of coffee with a smooth finish, providing the perfect start to your day or a refreshing break anytime.\n",
         "tagline": "Brand: Nescafe\nProduct Name: Classic Instant Coffee\nPackaging: Jar\nNet Weight: 95g\nIngredients: 100% instant coffee.\nFlavor Profile:\nClassic and balanced flavor with a smooth finish.\nRich aroma that awakens the senses.\nPreparation Method:\nAdd 1-2 teaspoons of instant coffee to a cup.\nPour hot water and stir well until dissolved. Enjoy your coffee!\nVersatility:\nIdeal for everyday coffee consumption.\nCan be enhanced with milk, cream, or sugar according to personal preference.",
